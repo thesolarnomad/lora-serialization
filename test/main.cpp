9a79e67699a4f236ff6fe7b2e8fc4a60e745335b
Joscha Feth <joscha@feth.com>
2016-07-14 21:17:47 +1000

    remove clutter from lib only branch

--- test/main.cpp
+++
@@ -1,5 +0,0 @@
-#include "main.h"
-
-#include "helpers.cpp"
-#include "../src/LoraEncoder.cpp"
-#include "test.cpp"
--- test/main.h
+++
@@ -1,6 +0,0 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "lib/Catch/single_include/catch.hpp"
-#ifndef Arduino_h
-    #include <stdint.h>
-    typedef uint8_t byte;
-#endif
