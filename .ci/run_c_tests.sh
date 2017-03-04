bfaa18b3d7da425e53dd218782dfe9c4348a659a
Joscha Feth <joscha@feth.com>
2017-03-04 17:56:53 +1100

    Coverage for C

--- .ci/run_c_tests.sh
+++ .ci/run_c_tests.sh
@@ -2,5 +2,17 @@
 set -eu
 
 pushd test > /dev/null
+make clean all
 make test
+lcov \
+  --test-name "loraSerialization" \
+  --output-file coverage.info \
+  --capture \
+  --directory . > /dev/null
+lcov \
+  --output-file coverage.info \
+  --extract coverage.info "**/src/*" > /dev/null
+lcov --list ./coverage.info
+mkdir -p ../coverage/C
+genhtml -o ../coverage/C/lcov-report coverage.info > /dev/null
 popd > /dev/null
